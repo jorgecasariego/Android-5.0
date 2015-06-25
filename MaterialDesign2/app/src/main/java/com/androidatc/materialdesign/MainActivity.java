fa98aa964886396cdd7dc850df5166e7a249bf06
Jorge Casariego <jorgekasa@gmail.com>
2015-06-25 00:34:11 -0400

    Android Navigation Drawer creation step by step

--- MaterialDesign2/app/src/main/java/com/androidatc/materialdesign/MainActivity.java
+++ MaterialDesign2/app/src/main/java/com/androidatc/materialdesign/MainActivity.java
@@ -2,6 +2,7 @@ package com.androidatc.materialdesign;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -31,7 +32,9 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar = (Toolbar)findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
-
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout)findViewById(R.id.drawer_layout), toolbar);
     }
 
     @Override
--- MaterialDesign2/app/src/main/java/com/androidatc/materialdesign/NavigationDrawerFragment.java
+++ MaterialDesign2/app/src/main/java/com/androidatc/materialdesign/NavigationDrawerFragment.java
@@ -1,8 +1,13 @@
 package com.androidatc.materialdesign;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,11 +18,32 @@ import android.view.ViewGroup;
  */
 public class NavigationDrawerFragment extends Fragment {
 
+    public static final String PREF_FILE_NAME = "testPref";
+    public static final String KEY_USER_LEARNED_DRAWER = "user_learned_drawer";
+
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+
+    private boolean mUserLearnedDrawer;
+    private boolean mFromSaveInstanceState;
+
+    private View containerView;
+
 
     public NavigationDrawerFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mUserLearnedDrawer = Boolean.valueOf(readToPreferences(getActivity(), KEY_USER_LEARNED_DRAWER, "false"));
+
+        if(savedInstanceState != null){
+            mFromSaveInstanceState = true;
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,4 +53,54 @@ public class NavigationDrawerFragment extends Fragment {
     }
 
 
+    public void setUp(int fragmentId, DrawerLayout drawerLayout, Toolbar toolbar) {
+        containerView = getActivity().findViewById(fragmentId);
+        mDrawerLayout = drawerLayout;
+        mDrawerToggle = new ActionBarDrawerToggle(getActivity(), drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close){
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+
+                //SI el usuario nunca vio el drawer entonces esta es la primera vez que ve
+                if(!mUserLearnedDrawer){
+                    mUserLearnedDrawer = true;
+                    saveToPreferences(getActivity(), KEY_USER_LEARNED_DRAWER, mUserLearnedDrawer +"");
+                }
+
+                getActivity().invalidateOptionsMenu();
+            }
+
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                getActivity().invalidateOptionsMenu();
+            }
+        };
+
+        if (!mUserLearnedDrawer && !mFromSaveInstanceState) {
+            mDrawerLayout.openDrawer(containerView);
+        }
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        mDrawerLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                mDrawerToggle.syncState(); 
+            }
+        });
+    }
+
+    public static void saveToPreferences(Context context, String preferencesName, String preferencesValues){
+        SharedPreferences sp = context.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString(preferencesName, preferencesValues);
+        editor.apply();
+    }
+
+    public static String readToPreferences(Context context, String preferencesName, String defaultValue){
+        SharedPreferences sp = context.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE);
+        return sp.getString(preferencesName, defaultValue);
+    }
 }
