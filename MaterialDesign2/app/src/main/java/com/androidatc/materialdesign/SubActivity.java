14292b6e6df9626de44406039e13fba135f9365a
Jorge Casariego <jorgekasa@gmail.com>
2015-06-24 21:40:49 -0400

    Adding Actions to Toolbar

--- MaterialDesign2/app/src/main/java/com/androidatc/materialdesign/SubActivity.java
+++ MaterialDesign2/app/src/main/java/com/androidatc/materialdesign/SubActivity.java
@@ -1,6 +1,5 @@
 package com.androidatc.materialdesign;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -8,36 +7,26 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-/**
- * Pasos para configurar ToolBar
- * 1. Usar Theme.AppCompat.Light.NoActionBar  para prevenir que el sistema muestre el action bar
- * 2. Definir app_bar.xml conteniendo un ToolBar
- * 3. Usar <include> en layout.xml para añadir el ToolBar
- * 4. Instanciar el toolbar usando findViewById dentro del Activity
- * 5. LLamar a setSupportActionBar y pasarle el objeto ToolBar
- * 6. Personalizar app:theme y app:popupTheme si es necesario
- * 7. Personzalizar las diferentes propiedades del ToolBar ya sea usando el objeto toolBar
- *    directamente o usando getSupportActionBar()
- */
-public class MainActivity extends AppCompatActivity {
+public class SubActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sub);
 
         toolbar = (Toolbar)findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
 
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_sub, menu);
         return true;
     }
 
@@ -51,10 +40,6 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
-        }
-
-        if (id == R.id.navigate) {
-            startActivity(new Intent(this, SubActivity.class));
         }
 
         return super.onOptionsItemSelected(item);
